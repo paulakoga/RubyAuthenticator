89c47903f8cc49f76f0a04c5a04dc8c224b93e6f
Paula Koga <69375172+paulakoga@users.noreply.github.com>
2021-10-08 02:41:05 +0000

    version 1 finished

--- main.rb
+++ main.rb
@@ -1,93 +1,48 @@
-# puts "Hello World"
-# p "Hello World"
+users = [
+          {username: "mashrur", password: "password1"},
+          {username: "jack", password: "password2"},
+          {username: "arya", password: "password3"},
+          {username: "jonsnow", password: "password4"},
+          {username: "heisenber", password: "password5"}        
+        ]
 
-# greeting = "Hello World"
-# puts greeting
-# first_name = ''
+puts "Welcome to the authenticator"
+25.times {print "-"}
+puts
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
 
-# def say_hello(thing_to_say)
-#   puts thing_to_say
-# end
+count = 0
 
-# say_hello 'Hello Paula'
+loop do
+  if count == 3
+    puts "You have exceeded the number of attempts"
+    break
+  end
+  count = count + 1
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
 
-#16
-# puts "What is your first name?"
-# first_name = gets.chomp
-# puts "What is your last name?"
-# last_name = gets.chomp
-# full_name = "#{first_name} #{last_name}"
-# puts "Your full name is " + full_name
-# puts "Your full name reversed is " + full_name.reverse!
-# count = first_name.length + last_name.length
-# puts "Your name has #{count} characters in it"
+  found = false
 
-#19
-# puts "Simple Calculator"
-# 25.times {print "-"}
-# puts
-# puts "Enter the first number"
-# num1 = gets.chomp
-# puts "Enter the second number"
-# num2 = gets.chomp
-# puts "The first number multiplied by the second number is #{num1.to_i * num2.to_i}"
-# puts "The first number divided by the second one is #{num1.to_f / num2.to_f}"
-# puts "The first number added by the second one is #{num1.to_i + num2.to_i}"
-# puts "The first number subtracted by the second one is #{num1.to_i - num2.to_i}"
-# puts "The first number divided by the second one has the remainder #{num1.to_i % num2.to_i}"
+  users.each do |x|
+    if username == x[:username] && password == x[:password]
+      puts x
+      found = true
+      break
+    else
+      found = false
+      next
+    end
+  end 
+  if found ==
+    false
+    puts "Credentials were not correct"
+  end
 
-
-#23
-# def multiply(first, second)
-#   first.to_f * second.to_f
-# end
-
-# puts "Simple Calculator"
-# 25.times {print "-"}
-# puts
-# puts "Enter the first number"
-# num1 = gets.chomp
-# puts "Enter the second number"
-# num2 = gets.chomp
-
-# puts "The first number multiplied by the second number is #{multiply(num1, num2)}"
-
-
-# #24
-# def multiply(first, second)
-#   first.to_f * second.to_f
-# end
-
-# def divide(first, second)
-#   first.to_f / second.to_f
-# end
-
-# def subtract(first, second)
-#   first.to_f - second.to_f
-# end
-
-# puts "Simple Calculator"
-# 25.times {print "-"}
-# puts
-# puts "Enter the first number"
-# num1 = gets.chomp
-# puts "Enter the second number"
-# num2 = gets.chomp
-# puts "What do you want to do?"
-# puts "Enter 1 for multiply, 2 for division and 3 for subtraction"
-# user_entry = gets.chomp
-# if user_entry == "1"
-#   puts "The first number multiplied by the second number is #{multiply(num1, num2)}"
-# elsif user_entry == "2"
-#   puts "The first number divided by the second one is #{divide(num1, num2)}"
-# elsif user_entry == "3"
-#   puts "The first number subtracted by the second one is #{subtract(num1, num2)}"
-# else
-#   puts "Invalid"
-# end
-
-sample_hash = {'a' => "test", 'b' => "test2"}
-
-sample_hash.each do |key, value|
-  puts "The key is #{key} and the value is #{value}"
+    puts "Press n to quit or any other key to continue"  
+    option = gets.chomp
+break if option == "n" 
 end
